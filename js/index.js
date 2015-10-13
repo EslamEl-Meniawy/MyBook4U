4455987026c2037132199472fb10935b9e6ecff7
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2015-10-13 16:47:56 +0200

    Initialize project

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/* 
+* @Author: Eslam El-Meniawy
+* @Date: 2015-10-13 16:44:14
+* @Last Modified by: eslam
+* @Last Modified time: 2015-10-13 16:44:14
+*
+* Dear maintainer:
+* When I wrote this, only God and I understood what I was doing
+* Now, God only knows
+* So, good luck maintaining the code :D
+*/
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {}
