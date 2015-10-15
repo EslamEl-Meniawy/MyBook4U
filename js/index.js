95ad1162d510f641e09d8e466bfdf34e764826af
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2015-10-15 14:54:27 +0200

    Add main page swiper

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 * @Author: Eslam El-Meniawy
 * @Date: 2015-10-13 16:44:14
 * @Last Modified by: eslam
-* @Last Modified time: 2015-10-15 10:17:22
+* @Last Modified time: 2015-10-15 14:53:26
 *
 * Dear maintainer:
 * When I wrote this, only God and I understood what I was doing
@@ -15,6 +15,16 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	$('.mdl-layout__drawer-button').html('<img class="material-icons" src="img/menu.png">');
 	document.addEventListener("backbutton", onBackKeyDown, false);
+	// Swiper slide width swiper-img
+	// Swiper
+	new Swiper('.swiper-container-main', {
+		pagination: '.swiper-pagination',
+		slidesPerView: 3,
+		spaceBetween: 8,
+		slidesPerGroup: 3,
+		paginationClickable: true,
+		loop: true
+	});
 }
 function onBackKeyDown() {
 	if ($('.mdl-layout__drawer').hasClass('is-visible')) {
